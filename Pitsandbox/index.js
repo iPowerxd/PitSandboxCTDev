76e0a7d367dac14fec324dfa3ac98d2d73dbb4c9
iPower/Zen <exileafterlife@gmail.com>
2022-08-23 20:18:58 -0400

    yes

--- Pitsandbox/index.js
+++ Pitsandbox/index.js
@@ -1689,7 +1689,6 @@ register("messageSent", (message, event) => {
     if (!message.startsWith("/")) {
 
         if (Settings.chatColor != "" && /^&.$/g.test(Settings.chatColor)) {
-            /* if (Player.getName() == "WolfyFurry" && message.startsWith("!")) return; */
             if (message.startsWith("\\")) return cancel(event), ChatLib.say(message.substring(1)), Client.getChatGUI().func_146239_a(message);
             if (Date.now() - lastunscramble > 35000 && Date.now() - lastquickmath > 35000) {
                 cancel(event);
