d6c2dbfc4b5aba358bf1e3ee9a00a692672d4247
JMB <jlock410@gmail.com>
2023-01-02 15:05:33 -0500

    Update index.js

--- PitSandboxDev/PitSandboxDev/index.js
+++ PitSandboxDev/PitSandboxDev/index.js
@@ -338,7 +338,7 @@ register("renderOverlay", () => {
     if (perks[1][0] != "Nothing") info.splice(5, 0, "&6" + (perks[1][0] == "Nothing" ? "" : perks[1][0]))
     if (perks[1][1] != "Nothing") info.splice(6, 0, "&6" + (perks[1][1] == "Nothing" ? "" : perks[1][1]))
     if (perks[1][2] != "Nothing") info.splice(7, 0, "&6" + (perks[1][2] == "Nothing" ? "" : perks[1][2]))
-    info.splice(8, 0, "&a" + perks[3][0] + (perks[3][1] == "None" ? "" : "&7 " + perks[3][1]))
+    //if (perks[3][0] != "Nothing") info.splice(8, 0, "&a" + perks[3][0] + (perks[3][1] == "None" ? "" : "&7 " + perks[3][1]))
     let y = 80
     info.forEach(line => {
         const text = new Text(line, 0, y)
